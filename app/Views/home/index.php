5bc49cece1a263b64e1cf2fd9356c64fb03a60d3
achmadrizky <rizky271092@gmail.com>
2022-07-11 12:32:54 +0700

    on progress home design

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -1,27 +1,154 @@
-<?php echo $this->extend('layout/template'); ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Home</title>
+  </head>
 
-<?php echo $this->section('content'); ?>
-
-<style>
-.max-w-screen-sm {
-    max-width: 480px!important;
-    background-color: #fff;
-    padding: 16px 16px 80px;
-}
-.mx-auto {
-    margin-left: auto!important;
-    margin-right: auto!important;
-}
-.min-h-full {
-    min-height: 100%!important;
-}
-</style>
-
-
-<main class="max-w-screen-sm mx-auto min-h-full">
-    <center>    
-        <h3> Hello, Alugada.co.id </h3>
-    </center>
-</main>
-
-<?php echo $this->endSection(); ?>
+  <style>
+    .mx-auto {
+      margin-left: auto !important;
+      margin-right: auto !important;
+    }
+    .min-h-full {
+      min-height: 100% !important;
+    }
+    .max-w-screen-sm {
+      max-width: 480px !important;
+    }
+    .hIYHVz {
+      --tw-bg-opacity: 1;
+      background-color: rgba(255, 255, 255, var(--tw-bg-opacity));
+      position: relative;
+      max-width: 480px;
+      width: 100%;
+      margin: 0px auto;
+      box-sizing: border-box;
+      /* top: 60px; */
+      padding: 0px 16px 80px;
+      min-height: calc(100vh - 60px);
+    }
+    .ehCkHB {
+      list-style-type: none;
+      display: inline-block;
+      border-width: 1px 0px 0px;
+      border-right-style: initial;
+      border-bottom-style: initial;
+      border-left-style: initial;
+      border-right-color: initial;
+      border-bottom-color: initial;
+      border-left-color: initial;
+      border-image: initial;
+      font-weight: 600;
+      line-height: 13px;
+      text-align: center;
+      vertical-align: middle;
+      transition: all 0.35s ease 0s;
+      text-decoration: none;
+      width: 100%;
+      max-width: 460px;
+      position: fixed;
+      bottom: 0px;
+      left: 50%;
+      transform: translate(-50%);
+      z-index: 2;
+      padding: 10px;
+      font-size: 1em;
+      border-top-style: solid;
+      border-top-color: rgb(222, 222, 222);
+      margin: 0px;
+      background-color: rgb(255, 255, 255);
+    }
+    .cwwuxL {
+        display: inline-block;
+        width: 19%;
+        font-size: 8px;
+        cursor: pointer;
+    }
+    }
+  </style>
+  <body style="background-color: #f7f7f7">
+    <main class="my-0 mx-auto min-h-full max-w-screen-sm">
+      <header>
+        <p>Header</p>
+      </header>
+      <div id="home-page" class="hIYHVz">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, esse
+          numquam. Accusamus repellendus et magnam alias dolores temporibus
+          totam iusto reiciendis modi porro ullam, atque earum, laudantium
+          dolorum officiis possimus?
+        </p>
+        <br />
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, esse
+          numquam. Accusamus repellendus et magnam alias dolores temporibus
+          totam iusto reiciendis modi porro ullam, atque earum, laudantium
+          dolorum officiis possimus?
+        </p>
+        <br />
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, esse
+          numquam. Accusamus repellendus et magnam alias dolores temporibus
+          totam iusto reiciendis modi porro ullam, atque earum, laudantium
+          dolorum officiis possimus?
+        </p>
+        <br />
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, esse
+          numquam. Accusamus repellendus et magnam alias dolores temporibus
+          totam iusto reiciendis modi porro ullam, atque earum, laudantium
+          dolorum officiis possimus?
+        </p>
+        <br />
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, esse
+          numquam. Accusamus repellendus et magnam alias dolores temporibus
+          totam iusto reiciendis modi porro ullam, atque earum, laudantium
+          dolorum officiis possimus?
+        </p>
+        <br />
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, esse
+          numquam. Accusamus repellendus et magnam alias dolores temporibus
+          totam iusto reiciendis modi porro ullam, atque earum, laudantium
+          dolorum officiis possimus?
+        </p>
+        <br />
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, esse
+          numquam. Accusamus repellendus et magnam alias dolores temporibus
+          totam iusto reiciendis modi porro ullam, atque earum, laudantium
+          dolorum officiis possimus?
+        </p>
+        <br />
+        <br />
+      </div>
+      <div class="ehCkHB">
+        <div class="cwwuxL">
+            <p>BERANDA</p>
+        </div>
+        <div class="cwwuxL">
+            <p>TENTANG KAMI</p>
+        </div>
+        <div class="cwwuxL">
+            <p>IKLAN</p>
+        </div>
+        <div class="cwwuxL">
+            <p>KONTAK KAMI</p>
+        </div>
+        <div class="cwwuxL">
+            <p>PESAN</p>
+        </div>
+      </div>
+    </main>
+  </body>
+</html>
