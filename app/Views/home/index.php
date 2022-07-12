49399cb4dc9bd4745db2dca8c6e658a499d3b21b
abdulrahman251201 <abdulrahman251201@gmail.com>
2022-07-12 10:22:11 +0700

    refactor include css

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Home</title>
 
-    <link rel="stylesheet" href="<?= base_url(); ?>/css/home.css">
-
+    <link rel="stylesheet" href="<?= base_url(); ?>/home/css/styles.css">
+    
     <!-- Slick style -->
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.3.11/slick.css"/>
   </head>
@@ -16,188 +16,197 @@
       <header>
         <p>Header</p>
       </header>
-      <div id="home-page" class="hIYHVz">
+      <div id="home-page" class="row-sm">
       <div class="fade">
         <div><img src="https://s3-ap-southeast-1.amazonaws.com/assets.segari.id/banners/it-1107IkanDori15000-l5fsmppo-l5fsr33t.jpg" /></div>
         <div><img src="https://s3-ap-southeast-1.amazonaws.com/assets.segari.id/banners/it-1107Buahserba15000-l5fsiuxd-l5fsunq2.jpg" /></div>
         <div><img src="https://s3.ap-southeast-1.amazonaws.com/assets.segari.id/customer-to-agent/01-Web+Banner+-+Customer+to+Mitra+Page+(1).jpg" /></div>
       </div>
 
-        <div class="CategoriesBar_cat-btn-group__LvhYW">
-          <button class="CategoriesBar_cat-btn__2Hkun CategoriesBar_categoryButtonActive__5HYRC" variant="light">
-            <div class="CategoriesBar_catBtnImage__1l0Se">
-              <img class="CategoriesBar_cat-btn-icon__2kVqF" src="https://s3-ap-southeast-1.amazonaws.com/assets.segari.id/categories/v3/semua_produk.png" alt="semua">
+        <div class="row-categories">
+          <button class="categories-button" variant="light">
+            <div>
+              <img class="img-categories" src="https://s3-ap-southeast-1.amazonaws.com/assets.segari.id/categories/v3/semua_produk.png" alt="semua">
             </div>
-            <div class="CategoriesBar_cat-btn-label__2zV_T">
+            <div class="label-categories">
               Tenaga Ahli
             </div>
           </button>
-          <button class="CategoriesBar_cat-btn__2Hkun CategoriesBar_categoryButtonActive__5HYRC" variant="light">
-            <div class="CategoriesBar_catBtnImage__1l0Se">
-              <img class="CategoriesBar_cat-btn-icon__2kVqF" src="https://s3.ap-southeast-1.amazonaws.com/assets.segari.id/categories/v3/ProdukBaru.png" alt="semua">
+          <button class="categories-button" variant="light">
+            <div>
+              <img class="img-categories" src="https://s3.ap-southeast-1.amazonaws.com/assets.segari.id/categories/v3/ProdukBaru.png" alt="semua">
             </div>
-            <div class="CategoriesBar_cat-btn-label__2zV_T">
+            <div class="label-categories">
               Tenaga Terampil
             </div>
           </button>
-          <button class="CategoriesBar_cat-btn__2Hkun CategoriesBar_categoryButtonActive__5HYRC" variant="light">
-            <div class="CategoriesBar_catBtnImage__1l0Se">
-              <img class="CategoriesBar_cat-btn-icon__2kVqF" src="https://s3-ap-southeast-1.amazonaws.com/assets.segari.id/categories/v3/sayuran-semua_not-selected.png" alt="semua">
+          <button class="categories-button" variant="light">
+            <div>
+              <img class="img-categories" src="https://s3-ap-southeast-1.amazonaws.com/assets.segari.id/categories/v3/sayuran-semua_not-selected.png" alt="semua">
             </div>
-            <div class="CategoriesBar_cat-btn-label__2zV_T">
+            <div class="label-categories">
               Property dijual
             </div>
           </button>
-          <button class="CategoriesBar_cat-btn__2Hkun CategoriesBar_categoryButtonActive__5HYRC" variant="light">
-            <div class="CategoriesBar_catBtnImage__1l0Se">
-              <img class="CategoriesBar_cat-btn-icon__2kVqF" src="https://s3-ap-southeast-1.amazonaws.com/assets.segari.id/categories/v3/buah-semua_not-selected.png" alt="semua">
+          <button class="categories-button" variant="light">
+            <div>
+              <img class="img-categories" src="https://s3-ap-southeast-1.amazonaws.com/assets.segari.id/categories/v3/buah-semua_not-selected.png" alt="semua">
             </div>
-            <div class="CategoriesBar_cat-btn-label__2zV_T">
+            <div class="label-categories">
               Property disewakan
             </div>
           </button>
-          <button class="CategoriesBar_cat-btn__2Hkun CategoriesBar_categoryButtonActive__5HYRC" variant="light">
-            <div class="CategoriesBar_catBtnImage__1l0Se">
-              <img class="CategoriesBar_cat-btn-icon__2kVqF" src="https://s3-ap-southeast-1.amazonaws.com/assets.segari.id/categories/v3/daging-semua_not-selected.png" alt="semua">
+          <button class="categories-button" variant="light">
+            <div>
+              <img class="img-categories" src="https://s3-ap-southeast-1.amazonaws.com/assets.segari.id/categories/v3/daging-semua_not-selected.png" alt="semua">
             </div>
-            <div class="CategoriesBar_cat-btn-label__2zV_T">
+            <div class="label-categories">
               Kost & Kontrakan
             </div>
           </button>
-          <button class="CategoriesBar_cat-btn__2Hkun CategoriesBar_categoryButtonActive__5HYRC" variant="light"><div class="CategoriesBar_catBtnImage__1l0Se"><img class="CategoriesBar_cat-btn-icon__2kVqF" src="https://s3-ap-southeast-1.amazonaws.com/assets.segari.id/categories/v3/semua_produk.png" alt="semua"></div>
-            <div class="CategoriesBar_cat-btn-label__2zV_T">
+          <button class="categories-button" variant="light">
+            <div>
+              <img class="img-categories" src="https://s3-ap-southeast-1.amazonaws.com/assets.segari.id/categories/v3/semua_produk.png" alt="semua">
+            </div>
+            <div class="label-categories">
               Mobil - Motor Dijual
             </div>
           </button>
-          <button class="CategoriesBar_cat-btn__2Hkun CategoriesBar_categoryButtonActive__5HYRC" variant="light"><div class="CategoriesBar_catBtnImage__1l0Se"><img class="CategoriesBar_cat-btn-icon__2kVqF" src="https://s3-ap-southeast-1.amazonaws.com/assets.segari.id/categories/v3/semua_produk.png" alt="semua"></div>
-            <div class="CategoriesBar_cat-btn-label__2zV_T">
+          <button class="categories-button" variant="light">
+            <div>
+              <img class="img-categories" src="https://s3-ap-southeast-1.amazonaws.com/assets.segari.id/categories/v3/semua_produk.png" alt="semua">
+            </div>
+            <div class="label-categories">
               Mobil Motor disewakan
             </div>
           </button>
-          <button class="CategoriesBar_cat-btn__2Hkun CategoriesBar_categoryButtonActive__5HYRC" variant="light"><div class="CategoriesBar_catBtnImage__1l0Se"><img class="CategoriesBar_cat-btn-icon__2kVqF" src="https://s3-ap-southeast-1.amazonaws.com/assets.segari.id/categories/v3/semua_produk.png" alt="semua"></div>
-            <div class="CategoriesBar_cat-btn-label__2zV_T">
+          <button class="categories-button" variant="light">
+            <div>
+              <img class="img-categories" src="https://s3-ap-southeast-1.amazonaws.com/assets.segari.id/categories/v3/semua_produk.png" alt="semua">
+            </div>
+            <div class="label-categories">
               Semua Layanan
             </div>
           </button>
       </div>
 
-        <div class="row-card">
-          <div class="ProductCard_productCard__fV4tL">
-            <div class="oTgHF">
-              <figure class="izvwwH">
-                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/bbhtuz5mby343-ID/image;s=300x0" class="hxopFq">
+        <div class="row-card-layanan">
+          <div class="card-layanan-list">
+            <div class="card-layanan">
+              <figure class="card-figure-layanan">
+                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/bbhtuz5mby343-ID/image;s=300x0" class="card-img-layanan">
               </figure>
-              <div class="cveMns">
-              <span class="xqVvp">2016, Daihatsu Ayla</span>
+              <div class="row-title-layanan">
+              <span class="title-layanan">2016, Daihatsu Ayla</span>
               </div>
             </div>
           </div>
-          <div class="ProductCard_productCard__fV4tL">
-            <div class="oTgHF">
-              <figure class="izvwwH">
-                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/n9qzfpcwwkyk3-ID/image;s=300x0" class="hxopFq">
+          <div class="card-layanan-list">
+            <div class="card-layanan">
+              <figure class="card-figure-layanan">
+                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/n9qzfpcwwkyk3-ID/image;s=300x0" class="card-img-layanan">
               </figure>
-              <div class="cveMns">
-              <span class="xqVvp">2015, Datsun Go+ Panca</span>
+              <div class="row-title-layanan">
+              <span class="title-layanan">2015, Datsun Go+ Panca</span>
               </div>
             </div>
           </div>
-          <div class="ProductCard_productCard__fV4tL">
-            <div class="oTgHF">
-              <figure class="izvwwH">
-                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="hxopFq">
+          <div class="card-layanan-list">
+            <div class="card-layanan">
+              <figure class="card-figure-layanan">
+                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="card-img-layanan">
               </figure>
-              <div class="cveMns">
-              <span class="xqVvp">2017, Daihatsu Ayla</span>
+              <div class="row-title-layanan">
+              <span class="title-layanan">2017, Daihatsu Ayla</span>
               </div>
             </div>
           </div>
-          <div class="ProductCard_productCard__fV4tL">
-            <div class="oTgHF">
-              <figure class="izvwwH">
-                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="hxopFq">
+          <div class="card-layanan-list">
+            <div class="card-layanan">
+              <figure class="card-figure-layanan">
+                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="card-img-layanan">
               </figure>
-              <div class="cveMns">
-              <span class="xqVvp">2017, Daihatsu Ayla</span>
+              <div class="row-title-layanan">
+              <span class="title-layanan">2017, Daihatsu Ayla</span>
               </div>
             </div>
           </div>
-          <div class="ProductCard_productCard__fV4tL">
-            <div class="oTgHF">
-              <figure class="izvwwH">
-                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="hxopFq">
+          <div class="card-layanan-list">
+            <div class="card-layanan">
+              <figure class="card-figure-layanan">
+                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="card-img-layanan">
               </figure>
-              <div class="cveMns">
-              <span class="xqVvp">2017, Daihatsu Ayla</span>
+              <div class="row-title-layanan">
+              <span class="title-layanan">2017, Daihatsu Ayla</span>
               </div>
             </div>
           </div>
-          <div class="ProductCard_productCard__fV4tL">
-            <div class="oTgHF">
-              <figure class="izvwwH">
-                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="hxopFq">
+          <div class="card-layanan-list">
+            <div class="card-layanan">
+              <figure class="card-figure-layanan">
+                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="card-img-layanan">
               </figure>
-              <div class="cveMns">
-              <span class="xqVvp">2017, Daihatsu Ayla</span>
+              <div class="row-title-layanan">
+              <span class="title-layanan">2017, Daihatsu Ayla</span>
               </div>
             </div>
           </div>
-          <div class="ProductCard_productCard__fV4tL">
-            <div class="oTgHF">
-              <figure class="izvwwH">
-                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="hxopFq">
+          <div class="card-layanan-list">
+            <div class="card-layanan">
+              <figure class="card-figure-layanan">
+                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="card-img-layanan">
               </figure>
-              <div class="cveMns">
-              <span class="xqVvp">2017, Daihatsu Ayla</span>
+              <div class="row-title-layanan">
+              <span class="title-layanan">2017, Daihatsu Ayla</span>
               </div>
             </div>
           </div>
-          <div class="ProductCard_productCard__fV4tL">
-            <div class="oTgHF">
-              <figure class="izvwwH">
-                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="hxopFq">
+          <div class="card-layanan-list">
+            <div class="card-layanan">
+              <figure class="card-figure-layanan">
+                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="card-img-layanan">
               </figure>
-              <div class="cveMns">
-              <span class="xqVvp">2017, Daihatsu Ayla</span>
+              <div class="row-title-layanan">
+              <span class="title-layanan">2017, Daihatsu Ayla</span>
               </div>
             </div>
           </div>
-          <div class="ProductCard_productCard__fV4tL">
-            <div class="oTgHF">
-              <figure class="izvwwH">
-                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="hxopFq">
+          <div class="card-layanan-list">
+            <div class="card-layanan">
+              <figure class="card-figure-layanan">
+                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="card-img-layanan">
               </figure>
-              <div class="cveMns">
-              <span class="xqVvp">2017, Daihatsu Ayla</span>
+              <div class="row-title-layanan">
+              <span class="title-layanan">2017, Daihatsu Ayla</span>
               </div>
             </div>
           </div>
-          <div class="ProductCard_productCard__fV4tL">
-            <div class="oTgHF">
-              <figure class="izvwwH">
-                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="hxopFq">
+          <div class="card-layanan-list">
+            <div class="card-layanan">
+              <figure class="card-figure-layanan">
+                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="card-img-layanan">
               </figure>
-              <div class="cveMns">
-              <span class="xqVvp">2017, Daihatsu Ayla</span>
+              <div class="row-title-layanan">
+              <span class="title-layanan">2017, Daihatsu Ayla</span>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="ehCkHB">
-        <div class="cwwuxL">
+      <div class="footer">
+        <div class="row-footer">
             <p>BERANDA</p>
         </div>
-        <div class="cwwuxL">
+        <div class="row-footer">
             <p>TENTANG KAMI</p>
         </div>
-        <div class="cwwuxL">
+        <div class="row-footer">
             <p>IKLAN</p>
         </div>
-        <div class="cwwuxL">
+        <div class="row-footer">
             <p>KONTAK KAMI</p>
         </div>
-        <div class="cwwuxL">
+        <div class="row-footer">
             <p>PESAN</p>
         </div>
       </div>
@@ -207,22 +216,7 @@
     <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.3.11/slick.min.js"></script>
-
-
-    <script type="text/javascript">
-        $(document).ready(function(){       
-          $('.fade').slick({
-              dots: true,
-              infinite: true,
-              speed: 700,
-              autoplay:true,
-              autoplaySpeed: 2000,
-              arrows:false,
-              slidesToShow: 1,
-              slidesToScroll: 1,
-          });
-        });
-</script>
+    <script type="text/javascript" src="<?= base_url(); ?>/home/js/custom.js"></script>
 
   </body>
 </html>
---
+++ public/home/js/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){       
+    $('.fade').slick({
+        dots: true,
+        infinite: true,
+        speed: 700,
+        autoplay:true,
+        autoplaySpeed: 2000,
+        arrows:false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    });
+  });
