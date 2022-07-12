6b9fe83c8d7693684d8547336e43f751b85310bf
harto <pakdeharto1967@gmail.com>
2022-07-12 14:54:51 +0700

    Implementasi data kategori

---
+++ app/Models/ModelAlugada.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ModelAlugada extends Model
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'modelalugadas';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $insertID         = 0;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+
+
+    public function userbynohp($nohp)
+    {
+        return $this->db->table('tbl_user')->getWhere(['nohp' => $nohp])->getRowArray();
+    }
+    public function userbyid($id)
+    {
+        return $this->db->table('tbl_user')->getWhere(['id' => $id])->getRowArray();
+    }
+
+    // Layanan
+    public function layanan()
+    {
+        return $this->db->table('tbl_layanan')->get()->getResultArray();
+    }
+    public function layananbyid($id)
+    {
+        return $this->db->table('tbl_layanan')->getWhere(['id' => $id])->getRowArray();
+    }
+    public function layananbynolayanan($nolayanan)
+    {
+        return $this->db->table('tbl_layanan')->getWhere(['nolayanan' => $nolayanan])->getRowArray();
+    }
+    public function tambahlayanan($data)
+    {
+        return $this->db->table('tbl_layanan')->insert($data);
+    }
+    public function updatelayanan($id, $data)
+    {
+        return $this->db->table('tbl_layanan')->update($data, ['id' => $id]);
+    }
+
+
+    // Sub Layanan
+    public function sublayanan()
+    {
+        return $this->db->table('tbl_sublayanan')->get()->getResultArray();
+    }
+    public function simpantambahsublayanan($data)
+    {
+        return $this->db->table('tbl_sublayanan')->insert($data);
+    }
+    public function sublayananbyid($id)
+    {
+        return $this->db->table('tbl_sublayanan')->getWhere(['id' => $id])->getRowArray();
+    }
+    public function updatesublayanan($id, $data)
+    {
+        return $this->db->table('tbl_sublayanan')->update($data, ['id' => $id]);
+    }
+
+    // New User
+    public function simpannewuser($data){
+        return $this->db->table('tbl_user')->insert($data);
+    }
+
+    public function jenisiklan(){
+        return $this->db->table('tbl_jenisiklan')->get()->getResultArray();
+    }
+    public function tambahjenisiklan($data){
+        return $this->db->table('tbl_jenisiklan')->insert($data);
+    }
+    public function jenisiklanbyid($id){
+        return $this->db->table('tbl_jenisiklan')->getWhere(['id'=>$id])->getRowArray();
+    }
+    public function updatejenisiklan($id,$data){
+        return $this->db->table('tbl_jenisiklan')->update($data, ['id' => $id]);
+    }
+
+}
--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -1,168 +1,176 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Home</title>
 
-    <link rel="stylesheet" href="<?= base_url(); ?>/home/css/styles.css">
-    
-    <!-- Slick style -->
-    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.3.11/slick.css"/>
-  </head>
-  <body style="background-color: #f7f7f7">
-    <main class="container-sm">
-      <header>
-        <p>Header</p>
-      </header>
-      <div id="home-page" class="row-sm">
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Home</title>
+
+  <link rel="stylesheet" href="<?= base_url(); ?>/home/css/styles.css">
+
+  <!-- Slick style -->
+  <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.3.11/slick.css" />
+</head>
+
+<body style="background-color: #f7f7f7">
+  <main class="container-sm">
+    <header>
+      <p>Header</p>
+    </header>
+    <div id="home-page" class="row-sm">
       <div class="fade">
         <div><img src="https://s3-ap-southeast-1.amazonaws.com/assets.segari.id/banners/it-1107IkanDori15000-l5fsmppo-l5fsr33t.jpg" /></div>
         <div><img src="https://s3-ap-southeast-1.amazonaws.com/assets.segari.id/banners/it-1107Buahserba15000-l5fsiuxd-l5fsunq2.jpg" /></div>
         <div><img src="https://s3.ap-southeast-1.amazonaws.com/assets.segari.id/customer-to-agent/01-Web+Banner+-+Customer+to+Mitra+Page+(1).jpg" /></div>
       </div>
 
-        <div class="row-categories">
-          <!-- // looping -->
+      <div class="row-categories">
+        <!-- // looping -->
+        <?php foreach ($layanan as $l) : ?>
           <button class="categories-button" variant="light">
             <div>
-              <img class="img-categories" src="https://s3-ap-southeast-1.amazonaws.com/assets.segari.id/categories/v3/semua_produk.png" alt="semua">
+              <!-- <img class="img-categories" src="https://s3-ap-southeast-1.amazonaws.com/assets.segari.id/categories/v3/semua_produk.png" alt="semua"> -->
+              <img class="img-categories" src="<?= base_url('Image/Layanan/' . $l['gambar']); ?>" alt="semua">
+
             </div>
             <div class="label-categories">
-              Tenaga <br /> Ahli
+              <p><?= $l['layanan']; ?></p>
+              <!-- Tenaga <br /> Ahli -->
             </div>
           </button>
-          <!-- // end looping -->
+        <?php endforeach; ?>
+        <!-- // end looping -->
       </div>
 
-        <div class="row-card-layanan">
-          <div class="card-layanan-list">
-            <div class="card-layanan">
-              <figure class="card-figure-layanan">
-                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/bbhtuz5mby343-ID/image;s=300x0" class="card-img-layanan">
-              </figure>
-              <div class="row-title-layanan">
+      <div class="row-card-layanan">
+        <div class="card-layanan-list">
+          <div class="card-layanan">
+            <figure class="card-figure-layanan">
+              <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/bbhtuz5mby343-ID/image;s=300x0" class="card-img-layanan">
+            </figure>
+            <div class="row-title-layanan">
               <span class="title-layanan">2016, Daihatsu Ayla</span>
-              </div>
             </div>
           </div>
-          <div class="card-layanan-list">
-            <div class="card-layanan">
-              <figure class="card-figure-layanan">
-                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/n9qzfpcwwkyk3-ID/image;s=300x0" class="card-img-layanan">
-              </figure>
-              <div class="row-title-layanan">
+        </div>
+        <div class="card-layanan-list">
+          <div class="card-layanan">
+            <figure class="card-figure-layanan">
+              <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/n9qzfpcwwkyk3-ID/image;s=300x0" class="card-img-layanan">
+            </figure>
+            <div class="row-title-layanan">
               <span class="title-layanan">2015, Datsun Go+ Panca</span>
-              </div>
             </div>
           </div>
-          <div class="card-layanan-list">
-            <div class="card-layanan">
-              <figure class="card-figure-layanan">
-                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="card-img-layanan">
-              </figure>
-              <div class="row-title-layanan">
+        </div>
+        <div class="card-layanan-list">
+          <div class="card-layanan">
+            <figure class="card-figure-layanan">
+              <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="card-img-layanan">
+            </figure>
+            <div class="row-title-layanan">
               <span class="title-layanan">2017, Daihatsu Ayla</span>
-              </div>
             </div>
           </div>
-          <div class="card-layanan-list">
-            <div class="card-layanan">
-              <figure class="card-figure-layanan">
-                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="card-img-layanan">
-              </figure>
-              <div class="row-title-layanan">
+        </div>
+        <div class="card-layanan-list">
+          <div class="card-layanan">
+            <figure class="card-figure-layanan">
+              <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="card-img-layanan">
+            </figure>
+            <div class="row-title-layanan">
               <span class="title-layanan">2017, Daihatsu Ayla</span>
-              </div>
             </div>
           </div>
-          <div class="card-layanan-list">
-            <div class="card-layanan">
-              <figure class="card-figure-layanan">
-                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="card-img-layanan">
-              </figure>
-              <div class="row-title-layanan">
+        </div>
+        <div class="card-layanan-list">
+          <div class="card-layanan">
+            <figure class="card-figure-layanan">
+              <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="card-img-layanan">
+            </figure>
+            <div class="row-title-layanan">
               <span class="title-layanan">2017, Daihatsu Ayla</span>
-              </div>
             </div>
           </div>
-          <div class="card-layanan-list">
-            <div class="card-layanan">
-              <figure class="card-figure-layanan">
-                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="card-img-layanan">
-              </figure>
-              <div class="row-title-layanan">
+        </div>
+        <div class="card-layanan-list">
+          <div class="card-layanan">
+            <figure class="card-figure-layanan">
+              <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="card-img-layanan">
+            </figure>
+            <div class="row-title-layanan">
               <span class="title-layanan">2017, Daihatsu Ayla</span>
-              </div>
             </div>
           </div>
-          <div class="card-layanan-list">
-            <div class="card-layanan">
-              <figure class="card-figure-layanan">
-                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="card-img-layanan">
-              </figure>
-              <div class="row-title-layanan">
+        </div>
+        <div class="card-layanan-list">
+          <div class="card-layanan">
+            <figure class="card-figure-layanan">
+              <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="card-img-layanan">
+            </figure>
+            <div class="row-title-layanan">
               <span class="title-layanan">2017, Daihatsu Ayla</span>
-              </div>
             </div>
           </div>
-          <div class="card-layanan-list">
-            <div class="card-layanan">
-              <figure class="card-figure-layanan">
-                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="card-img-layanan">
-              </figure>
-              <div class="row-title-layanan">
+        </div>
+        <div class="card-layanan-list">
+          <div class="card-layanan">
+            <figure class="card-figure-layanan">
+              <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="card-img-layanan">
+            </figure>
+            <div class="row-title-layanan">
               <span class="title-layanan">2017, Daihatsu Ayla</span>
-              </div>
             </div>
           </div>
-          <div class="card-layanan-list">
-            <div class="card-layanan">
-              <figure class="card-figure-layanan">
-                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="card-img-layanan">
-              </figure>
-              <div class="row-title-layanan">
+        </div>
+        <div class="card-layanan-list">
+          <div class="card-layanan">
+            <figure class="card-figure-layanan">
+              <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="card-img-layanan">
+            </figure>
+            <div class="row-title-layanan">
               <span class="title-layanan">2017, Daihatsu Ayla</span>
-              </div>
             </div>
           </div>
-          <div class="card-layanan-list">
-            <div class="card-layanan">
-              <figure class="card-figure-layanan">
-                <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="card-img-layanan">
-              </figure>
-              <div class="row-title-layanan">
+        </div>
+        <div class="card-layanan-list">
+          <div class="card-layanan">
+            <figure class="card-figure-layanan">
+              <img alt="Sedekah Makan Yatim untuk Ratusan Panti Asuhan" src="https://apollo-singapore.akamaized.net:443/v1/files/6i5j1nbmmcgm3-ID/image;s=300x0" class="card-img-layanan">
+            </figure>
+            <div class="row-title-layanan">
               <span class="title-layanan">2017, Daihatsu Ayla</span>
-              </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="footer">
-        <div class="row-footer">
-            <p>BERANDA</p>
-        </div>
-        <div class="row-footer">
-            <p>TENTANG KAMI</p>
-        </div>
-        <div class="row-footer">
-            <p>IKLAN</p>
-        </div>
-        <div class="row-footer">
-            <p>KONTAK KAMI</p>
-        </div>
-        <div class="row-footer">
-            <p>PESAN</p>
-        </div>
+    </div>
+    <div class="footer">
+      <div class="row-footer">
+        <p>BERANDA</p>
       </div>
-    </main>
+      <div class="row-footer">
+        <p>TENTANG KAMI</p>
+      </div>
+      <div class="row-footer">
+        <p>PASANG IKLAN</p>
+      </div>
+      <div class="row-footer">
+        <p>KONTAK KAMI</p>
+      </div>
+      <div class="row-footer">
+        <p>PESAN</p>
+      </div>
+    </div>
+  </main>
 
 
-    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-    <script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.3.11/slick.min.js"></script>
-    <script type="text/javascript" src="<?= base_url(); ?>/home/js/custom.js"></script>
+  <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+  <script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.3.11/slick.min.js"></script>
+  <script type="text/javascript" src="<?= base_url(); ?>/home/js/custom.js"></script>
 
-  </body>
+</body>
+
 </html>
