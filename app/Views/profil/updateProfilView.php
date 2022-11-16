b79ff4d2623dc23d2737c5097ba5edf09b2ff058
Abdul Rahman <abdulrahman251201@gmail.com>
2022-11-16 16:52:56 +0700

    Implementasi Update Profil

--- app/Views/profil/updateProfilView.php
+++ app/Views/profil/updateProfilView.php
@@ -2,16 +2,26 @@
 
 <?= $this->section('content'); ?>
 <div id="home-page" class="row-sm padding-bottom-90">
-  <p class="margin-left-16 font-size-18 textcolor-303235  font-weight: 600;">Hy, Achmad Rizky</p>
-  <p class="margin-left-16 font-size-13 textcolor-303235 position-relative top-min14"> <i class="fa fa-edit"></i> Ubah Profil</p>
+  <p class="margin-left-16 font-size-18 textcolor-303235  font-weight: 600;">Updated Profil Data</p>
+  <p class="margin-left-16 font-size-13 textcolor-303235 position-relative top-min14">Silahkan isi data anda dengan lengkap</p>
   <div class="row">
     <div class="margin-bottom-12 padding-right-46 padding-left-16">
-      <label for="nohp" class="font-size-13">Masukkan Nomor Handphone <span class="textcolor-red"> *</span></label>
-      <input type="text" id="no_handphone" class="form-control margin-top-6" placeholder="08xxx">
+      <label for="nohp" class="font-size-13">Nama Lengkap</label>
+      <input type="text" id="nama_lengkap" class="form-control margin-top-6">
     </div>
     <div class="margin-bottom-12 padding-right-46 padding-left-16">
-      <label for="password" class="font-size-13">Masukkan Password <span class="textcolor-red"> *</span></label>
-      <input type="password" id="password" class="form-control margin-top-6" placeholder="***">
+      <label for="password" class="font-size-13">Email</label>
+      <input type="email" id="email" class="form-control margin-top-6" >
+    </div>
+    <div class="margin-bottom-12 padding-right-46 padding-left-16">
+      <label for="password" class="font-size-13">Alamat</label>
+      <!-- <input type="password" id="password" class="form-control margin-top-6" rd"> -->
+      <textarea name="" id="" class="form-control margin-top-6"></textarea>
+    </div>
+    <div class="margin-bottom-12 padding-right-46 padding-left-16">
+      <label for="password" class="font-size-13">Deskripsi</label>
+      <!-- <input type="password" id="password" class="form-control margin-top-6" rd"> -->
+      <textarea name="" id="" class="form-control margin-top-6"></textarea>
     </div>
     <div class="margin-bottom-12 padding-right-20 padding-left-16 margin-top-20">
       <button id="submit" class="btn btn-primary width-100-percent textcolor-white"> Submit </button>
