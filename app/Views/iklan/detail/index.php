cbfa8ea94eacbb05ca197b6c42266ad1a16a6b19
achmadrizky <rizky271092@gmail.com>
2022-12-06 12:34:25 +0700

    delete image

--- app/Views/iklan/detail/index.php
+++ app/Views/iklan/detail/index.php
@@ -1,15 +1,16 @@
 <?= $this->extend('layout/template'); ?>
 
 <?= $this->section('content'); ?>
-<div id="home-page" class="row-sm">
-  <div class="position-relative top10">
-    <div class="fade">
-      <div><img src="https://i.ytimg.com/vi/f_fC54rC9hc/maxresdefault.jpg" /></div>
-      <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkihA1QQqZxNnO83gFHJB3FJdZVOxC-hsYZA&usqp=CAU" /></div>
-      <div><img src="https://i.ytimg.com/vi/vql_cCL-YLE/maxresdefault.jpg" /></div>
-    </div>
-  </div>
 
+<div>
+  <div class="fade">
+    <div><img src="https://i.ytimg.com/vi/f_fC54rC9hc/maxresdefault.jpg" /></div>
+    <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkihA1QQqZxNnO83gFHJB3FJdZVOxC-hsYZA&usqp=CAU" /></div>
+    <div><img src="https://i.ytimg.com/vi/vql_cCL-YLE/maxresdefault.jpg" /></div>
+  </div>
+</div>
+
+<div id="home-page" class="row-sm">
   <?= $detail_iklan ? $this->include($detail_iklan) : null; ?>
 </div>
 
