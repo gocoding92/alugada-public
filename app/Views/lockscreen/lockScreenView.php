d06f68f4505edbd55d33d0da4c82ab1b2f602dfc
achmadrizky <rizky271092@gmail.com>
2022-12-15 14:59:06 +0700

    lock screen

--- app/Views/lockscreen/lockScreenView.php
+++ app/Views/lockscreen/lockScreenView.php
@@ -1,18 +1,22 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
 <div id="home-page" class="row-sm padding-bottom-90">
   <p class="margin-left-16 font-size-18 textcolor-303235  font-weight: 600;">Selamat datang di Alugada!</p>
-  <p class="margin-left-16 font-size-13 textcolor-303235 position-relative top-min14"> <i class="fa fa-user"></i> Silahkan masukkan pasword</p>
+  <p class="margin-left-16 font-size-13 textcolor-303235 position-relative top-min14"> <i class="fa fa-user"></i> Silahkan masukkan password</p>
   <div class="row">
     <div class="margin-bottom-12 padding-right-46 padding-left-16">
       <label for="password" class="font-size-13">Masukkan Password <span class="textcolor-red"> *</span></label>
       <input type="password" id="password" class="form-control margin-top-6" placeholder="***">
     </div>
-    <p class=" margin-left-16 margin-right-18 text-align-right">
-      <a class="textcolor-303235 font-size-14 text-decoration-none" href="<?php echo base_url('lupa-password'); ?>">Lupa Password ?</a>
-    </p>
     <div class="margin-bottom-12 padding-right-20 padding-left-16 margin-top-20">
-      <button id="submit" class="btn btn-primary width-100-percent textcolor-white"> Submit </button>
+      <button id="submit" class="btn btn-primary width-100-percent textcolor-white"> Login </button>
     </div>
+    </form>
   </div>
+
 </div>
 
 <?= $this->include("script/lockscreen"); ?>
+
+<?= $this->endSection(); ?>
