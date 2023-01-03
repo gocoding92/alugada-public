7d03bd6db582aa2acb244e34d111155dbc95becc
achmadrizky <rizky271092@gmail.com>
2023-01-03 14:31:47 +0700

    On Progress redirect wa iklan detail

--- app/Controllers/Iklan.php
+++ app/Controllers/Iklan.php
@@ -41,6 +41,7 @@ class Iklan extends BaseController
     public function detail($layanan = '', $no_layanan = '', $sublayanan = '', $no_sublayanan = '')
     {
         $this->form = '';
+        
 
         if ($layanan) {
             if ($layanan == 'Tenaga Ahli') {
@@ -74,7 +75,6 @@ class Iklan extends BaseController
                 $this->form = 'iklan/form/bangunan_komersial';
             }
         }
-
 
         $nohppengunjung = $this->session->get('nohppengunjung');
         if ($nohppengunjung == null) {
@@ -1242,5 +1242,37 @@ class Iklan extends BaseController
         $result['path_folder'] = $type_iklan;
 
         return view('iklan/edit-iklan/index', $result);
+    }
+
+    public function detailIklanWa() {
+        
+        $no_hp = $this->request->getVar('nohp');
+        
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.fonnte.com/send',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => array(
+                'target' => $no_hp,
+                'message' => "TEST IKLAN WA",
+                'url' => 'https://md.fonnte.com/images/wa-logo.png',
+                'filename' => 'filename',
+                'schedule' => '0',
+                'delay' => '2',
+                'countryCode' => '62',
+            ),
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: 2D3-nhz3YnAucN2D_Z4E'
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
     }
 }
--- app/Views/iklan/detail/index.php
+++ app/Views/iklan/detail/index.php
@@ -29,9 +29,40 @@
 
 <div id="home-page" class="row-sm">
   <?= $component_iklan ? $this->include($component_iklan) : null; ?>
-  <div>
+  <div onclick="redirecWhatshap()">
     <img class="icon-wa-detail-iklan" src="<?= base_url('Image/wa.png') ?>" />
   </div>
 </div>
+
+<script>
+  function redirecWhatshap() {
+    var session = <?= $_SESSION['nohp']; ?>
+
+    if (!session) {
+      window.location.href = "<?php echo base_url('/login'); ?>";
+    }
+
+    if (session) {
+        if (confirm("Apakah anda yakin untuk menghubungi via whatshapp ?") == true) {
+          $.ajax({
+                url: "<?= base_url('iklan/detail/whatshapp'); ?>",
+                type: 'POST',
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest'
+                },
+                data: {nohp: session},
+                success: function(data) {
+
+                  console.log('data', data);
+                  // toastr.success('Anda berhasil menghubungi pemilik iklan, tunggu balasan dari pemilik iklan!');
+
+                  return;
+                }
+            });
+
+        }
+    }
+  }
+</script>
 
 <?= $this->endSection(); ?>
