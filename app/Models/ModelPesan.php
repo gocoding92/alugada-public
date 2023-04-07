2e132a8ffa14505c4e222dc513134fdb78eaa3f5
achmadrizky <rizky271092@gmail.com>
2023-04-07 11:10:22 +0700

    fix order by list pesan

--- app/Models/ModelPesan.php
+++ app/Models/ModelPesan.php
@@ -12,6 +12,7 @@ class ModelPesan extends Model
                 join('tbl_user AS b', 'a.id_users_pengiklan = b.id')->
                 join('tbl_rekomendasi_iklan AS c', 'c.id_rekomendasi_iklan = a.id_rekomendasi_iklan')->
                 select('a.id, a.chat, a.created_time, b.gambar, b.nama, b.nohp, c.type_rekomendasi_iklan, c.nama_iklan, c.id_iklan, c.table_iklan, c.id_rekomendasi_iklan ')->
+                orderBy('a.id', 'DESC')->
                 getWhere(['a.id_users' => $id_user])->
                 getResultArray();
 
